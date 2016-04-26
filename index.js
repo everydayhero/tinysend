f3ff8220850b69a1750a0237ac74e5adc0bea48d
Ellis Clayton <ellis@ellisclayton.info>
2016-04-26 14:01:45 +1000

    Add Mandrill send. Verify valid origin by downloading donation receipt

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var mandrill = require('mandrill-api/mandrill')
+var fetch = require('node-fetch')
 
 var app = express()
 var jsonParser = bodyParser.json()
 
+var mandrillClient = new mandrill.Mandrill(process.env.MANDRILL_KEY)
+
 app.post('/send-email', jsonParser, function (req, res) {
-  res.send(req.hostname) // Req.hostname needs to be compared to a whitelist. If exists, send the email :D
+  fetch('https://heroix.everydayhero.com.au/donations/' + req.body.entity.id + '.pdf?token=' + req.body.entity.token)
+    .then(function(response) {
+      if (response.status < 300) {
+        return true
+      } else {
+        return false
+      }
+    }).then(function(okToSend) {
+      if (okToSend) {
+        mandrillClient.messages.sendTemplate({
+          template_name: req.body.template_name,
+          template_content: [],
+          message: {
+            global_merge_vars: [{
+              name: 'donorName',
+              content: req.body.entity.donor_name
+            }],
+            subject: req.body.entity.subject,
+            from_email: 'help@everydayhero.com.au',
+            from_name: req.body.entity.sender_name,
+            to: [{email: req.body.entity.email_address}],
+            inline_css: true,
+            preserve_recipients: false,
+          }
+        }, function(response) {
+          return res.sendStatus(200)
+        }, function(err) {
+          return res.sendStatus(422)
+        })
+      } else {
+        return res.sendStatus(422)
+      }
+    })
 })
 
 app.listen(3000, function () {
